28d99a3e0389dcab7548c28c8bb0db1fbc46c2c7
lizhaowen72 <794112629@qq.com>
2019-12-10 22:08:56 +0800

    提交

---
+++ src/main/java/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,31 @@
+package leetcode.editor.cn;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/12/9 23:39
+ * @Description: TODO
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
