f0ce4cffb72b845bba89fc1b7ba74843663ad41d
lizhaowen72 <794112629@qq.com>
2019-12-20 10:09:43 +0800

    12月17号晚上提交代码

--- src/main/java/leetcode/editor/cn/SimplifyPath.java
+++ src/main/java/leetcode/editor/cn/SimplifyPath.java
@@ -1,5 +1,8 @@
 package leetcode.editor.cn;
-//以 Unix 风格给出一个文件的绝对路径，你需要简化它。或者换句话说，将其转换为规范路径。 
+
+import java.util.*;
+
+//以 Unix 风格给出一个文件的绝对路径，你需要简化它。或者换句话说，将其转换为规范路径。
 //
 // 在 Unix 风格的文件系统中，一个点（.）表示当前目录本身；此外，两个点 （..） 表示将目录切换到上一级（指向父目录）；两者都可以是复杂相对路径的组成部分。更多信息请参阅：Linux / Unix中的绝对路径 vs 相对路径 
 //
@@ -45,18 +48,31 @@ package leetcode.editor.cn;
 // 输入："/a//b////c/d//././/.."
 //输出："/a/b/c" 
 // Related Topics 栈 字符串
-public class SimplifyPath{
+public class SimplifyPath {
     public static void main(String[] args) {
-         Solution solution = new SimplifyPath().new Solution();
+        Solution solution = new SimplifyPath().new Solution();
     }
-    
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String simplifyPath(String path) {
-        return null;
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String simplifyPath(String path) {
+            Deque<String> stack = new LinkedList<>();
+            Set<String> skip = new HashSet<>(Arrays.asList("", ".", ".."));
+            for (String dir : path.split("/")) {
+                if (dir.equals("..") && !stack.isEmpty()) {
+                    stack.pop();
+                } else if (!skip.contains(dir)) {
+                    stack.push(dir);
+                }
+            }
+            String res = "";
+            for (String dir : stack) {
+                res = "/" + dir + res;
+            }
+            return res.isEmpty() ? "/" : res;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
