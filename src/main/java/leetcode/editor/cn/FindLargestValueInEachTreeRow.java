f55d0fd06463502fcf861940e83231025e8fe9b8
lizhaowen72 <794112629@qq.com>
2020-01-06 22:49:08 +0800

    1.6提交代码

---
+++ src/main/java/leetcode/editor/cn/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,111 @@
+package leetcode.editor.cn;
+//您需要在二叉树的每一行中找到最大的值。 
+//
+// 示例： 
+//
+// 
+//输入: 
+//
+//          1
+//         / \
+//        3   2
+//       / \   \  
+//      5   3   9 
+//
+//输出: [1, 3, 9]
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class FindLargestValueInEachTreeRow {
+    public static void main(String[] args) {
+        Solution solution = new FindLargestValueInEachTreeRow().new Solution();
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(9);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+        node2.right = node6;
+        solution.largestValues(node1);
+    }
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        // bfs
+        public List<Integer> largestValues(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            List<Integer> res = new ArrayList<>();
+            if (root != null) {
+                queue.offer(root);
+            }
+            while (!queue.isEmpty()) {
+                int max = Integer.MIN_VALUE;
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    max = Math.max(max, node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                res.add(max);
+            }
+            return res;
+        }
+
+        // dfs
+        public List<Integer> largestValues2(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            dfs(root, res, 0);
+            return res;
+        }
+
+        private void dfs(TreeNode root, List<Integer> res, int d) {
+            if (root == null) return;
+            // expand list size
+            if (d == res.size()) {
+                res.add(root.val);
+            } else {// or set value
+                res.set(d, Math.max(res.get(d), root.val));
+            }
+            dfs(root.left, res, d + 1);
+            dfs(root.right, res, d + 1);
+        }
+
+
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
---
+++ src/main/java/leetcode/editor/queue/FindBottomLeftTreeValue.java
@@ -0,0 +1,94 @@
+package leetcode.editor.queue;
+//给定一个二叉树，在树的最后一行找到最左边的值。 
+//
+// 示例 1: 
+//
+// 
+//输入:
+//
+//    2
+//   / \
+//  1   3
+//
+//输出:
+//1
+// 
+//
+// 
+//
+// 示例 2: 
+//
+// 
+//输入:
+//
+//        1
+//       / \
+//      2   3
+//     /   / \
+//    4   5   6
+//       /
+//      7
+//
+//输出:
+//7
+// 
+//
+// 
+//
+// 注意: 您可以假设树（即给定的根节点）不为 NULL。 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindBottomLeftTreeValue {
+    public static void main(String[] args) {
+        Solution solution = new FindBottomLeftTreeValue().new Solution();
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        node1.left = node2;
+        node1.right = node3;
+        solution.findBottomLeftValue(node1);
+    }
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int findBottomLeftValue(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                root = queue.poll();
+                if (root.right != null) {
+                    queue.add(root.right);
+                }
+                if (root.left != null) {
+                    queue.add(root.left);
+                }
+            }
+            return root.val;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
